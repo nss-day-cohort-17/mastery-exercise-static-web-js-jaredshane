e20edd2af3e56e536c4f66b55c50072c2556cc64
Jared Fuller <jaredshane@gmail.com>
2016-12-02 14:34:43 -0600

    made changes and changed back to index.html, added function that will pprint the amount of characters to the console

--- script.js
+++ script.js
@@ -1,30 +1,16 @@
-/* Create a `tree` function that should build a pine tree out of a character in the Chrome dev tools console. */
 
-/* It accepts a single object as an argument. The object should have two key/value pairs. */
-
-/* A key that specifies the height of the pine tree. */
-
-/* The value for the height of the tree should be from user input in a `<input type="text">` field in the DOM.*/
-
-/*A key that specifies which character to use to build the pine tree. */
-
-/* The character to use should be from user input in a `<input type="text">` field in the DOM. */
-
-/*Once the user enters in a number, and a character, the user can either then just press the enter key _(as long as the cursor is in one of the input fields)_, or click a button that is labeled "Grow your tree" and the tree should be shown in the console. This requires you to add an event listener to the button, as well as an event listener for the enter/return key.*/
-
-/*If either of the input fields does not have a value in it when the user presses the enter key, or presses the button, then display an alert stating that both fields must have a value. */
 var inputTall = document.getElementById("inputTall");
 var inputCharacter = document.getElementById("inputCharacter");
-var button = document.getElementById("btn");
 
+document.querySelector('form.form').addEventListener('submit',
+    function treeFunc(event) {
+      //prevent the normal submission of the form
 
+      event.preventDefault();
 
-// var nameInput = document.getElementById('name');
-
-document.querySelector('form.form').addEventListener('submit', function (e) {
-
-    //prevent the normal submission of the form
-    e.preventDefault();
-
-    console.log(inputTall.value + inputCharacter.value);
-});
+      var num = 1;
+      while(num <= parseInt(inputTall.value)){
+        console.log(inputCharacter.value.repeat(num));
+        num++;
+      }
+  });
